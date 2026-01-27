16d4e9f684cc43910bada5124e5fdc813111931d
Vidhi <vidhipratheesh@gmail.com>
2026-01-27 17:01:23 +0530

    setup lensing ML environment

---
+++ lensing/experiments.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import cv2
+
+print("NumPy version:",np.__version__)
+print("Matplotlib ready")
+print("TensorFlow version:",tf.__version__)
+print("OpenCV version:",cv2.__version__)
+
+print("Lensing environment ready 🚀")
