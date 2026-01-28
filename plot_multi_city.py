8ca1536a08d6ccc06c9cfc168daf48d1ef27df6d
Vidhi <vidhipratheesh@gmail.com>
2026-01-28 21:55:54 +0530

    Plotted graph with csv data

---
+++ plot_multi_city.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df=pd.read_csv("weather_data.csv")
+
+df["datetime"]=pd.to_datetime(df["date"]+" "+df["time"])
+
+df=df.sort_values("datetime")
+
+cities=df["city"].unique()
+
+plt.figure()
+
+for city in cities:
+    city_data=df[df["city"]==city]
+    plt.plot(city_data["datetime"],city_data["temp"],label=city)
+
+plt.xlabel("Time")
+plt.ylabel("Temperature (°C)")
+plt.title("Temperature Trends (All Cities)")
+plt.legend()
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
