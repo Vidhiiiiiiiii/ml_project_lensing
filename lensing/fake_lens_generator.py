4be8294625668ea2164c605fc2783b006e61e8fd
Vidhi <vidhipratheesh@gmail.com>
2026-02-01 13:17:42 +0530

    Add fake lens dataset and first CNN classifier

---
+++ lensing/fake_lens_generator.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+os.makedirs("lensing/data/lensed",exist_ok=True)
+os.makedirs("lensing/data/unlensed",exist_ok=True)
+
+def generate_lensed(size=128):
+    img=np.zeros((size,size))
+
+    cx,cy=np.random.randint(40,88,size=2)
+
+    for i in range(size):
+        for j in range(size):
+            dist=np.sqrt((i-cx)**2 + (j-cy)**2)
+            if 18<dist<23:
+                img[i,j]=1
+
+    img+=np.random.normal(0,0.05,img.shape)
+
+    return img
+
+def generate_unlensed(size=128):
+    img=np.zeros((size,size))
+     
+    cx,cy=size//2,size//2
+
+    for i in range(size):
+        for j in range(size):
+            dist=np.sqrt((i-cx)**2 + (j-cy)**2)
+
+            if dist<15:
+                img[i,j]=1
+
+    img +=np.random.normal(0,0.05,img.shape)
+    return img
+NUM_IMAGES = 50
+
+for i in range(NUM_IMAGES):
+    lens=generate_lensed()
+    normal=generate_unlensed()
+
+    plt.imsave(f"lensing/data/lensed/lens_{i}.png",lens,cmap="inferno")
+    plt.imsave(f"lensing/data/unlensed/normal_{i}.png",normal,cmap="inferno")
+
+print("Fake lensing dataset created 🚀")
+
---
+++ lensing/train_cnn.py
@@ -0,0 +1,61 @@
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D,MaxPooling2D,Flatten,Dense
+import os
+
+DATA_DIR="lensing/data"
+IMG_SIZE=128
+BATCH_SIZE=8
+
+datagen=ImageDataGenerator(
+    rescale=1./255,
+    validation_split=0.2
+)
+
+train_data=datagen.flow_from_directory(
+    DATA_DIR,
+    target_size=(IMG_SIZE,IMG_SIZE),
+    batch_size=BATCH_SIZE,
+    class_mode="binary",
+    subset="training"
+)
+
+val_data=datagen.flow_from_directory(
+    DATA_DIR,
+    target_size=(IMG_SIZE,IMG_SIZE),
+    batch_size=BATCH_SIZE,
+    class_mode="binary",
+    subset="validation"
+)
+
+model=Sequential([
+    Conv2D(16,(3,3), activation="relu", input_shape=(IMG_SIZE,IMG_SIZE,3)),
+    MaxPooling2D(2,2),
+
+    Conv2D(32,(3,3), activation="relu"),
+    MaxPooling2D(2,2),
+
+    Flatten(),
+
+    Dense(64, activation="relu"),
+    Dense(1, activation="sigmoid")
+])
+
+model.compile(
+    optimizer="adam",
+    loss="binary_crossentropy",
+    metrics=["accuracy"]
+)
+
+model.summary()
+
+history=model.fit(
+    train_data,
+    validation_data=val_data,
+    epochs=10
+)
+
+model.save("lensing/lens_classifier.h5")
+
+print("Model trained & saved 🚀")
