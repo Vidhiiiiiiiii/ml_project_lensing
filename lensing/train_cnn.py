61be346182c019060c1a9e917e83de3cde2735b0
Vidhi <vidhipratheesh@gmail.com>
2026-02-08 16:54:50 +0530

    Improve CNN with regulations and data augmentation

--- lensing/train_cnn.py
+++ lensing/train_cnn.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D,MaxPooling2D,Flatten,Dense
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.layers import Conv2D,MaxPooling2D,Flatten,Dense,Dropout,BatchNormalization
 import os
 
 DATA_DIR="lensing/data"
@@ -10,7 +11,16 @@ BATCH_SIZE=8
 
 datagen=ImageDataGenerator(
     rescale=1./255,
-    validation_split=0.2
+    validation_split=0.2,
+
+    rotation_range=30,
+    zoom_range=0.3,
+    shear_range=0.2,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+
+    horizontal_flip=True,
+    brightness_range=[0.7,1.3]
 )
 
 train_data=datagen.flow_from_directory(
@@ -29,18 +39,30 @@ val_data=datagen.flow_from_directory(
     subset="validation"
 )
 
+
 model=Sequential([
-    Conv2D(16,(3,3), activation="relu", input_shape=(IMG_SIZE,IMG_SIZE,3)),
+    Conv2D(32,(3,3), activation="relu", input_shape=(IMG_SIZE,IMG_SIZE,3)),
+    BatchNormalization(),
     MaxPooling2D(2,2),
 
-    Conv2D(32,(3,3), activation="relu"),
+    Conv2D(64,(3,3), activation="relu"),
+    BatchNormalization(),
     MaxPooling2D(2,2),
+
+    # Conv2D(128,(3,3), activation="relu"),
+    # BatchNormalization(),
+    # MaxPooling2D(2,2),
 
     Flatten(),
 
+    # Dense(128, activation="relu"),
+    # Dropout(0.5),
     Dense(64, activation="relu"),
+    Dropout(0.4),
+
     Dense(1, activation="sigmoid")
 ])
+
 
 model.compile(
     optimizer="adam",
@@ -50,10 +72,17 @@ model.compile(
 
 model.summary()
 
+early_stop=EarlyStopping(
+    monitor="val_loss",
+    patience=3,
+    restore_best_weights=True
+)
+
 history=model.fit(
     train_data,
     validation_data=val_data,
-    epochs=10
+    epochs=20,
+    callbacks=[early_stop]
 )
 
 model.save("lensing/lens_classifier.keras")
