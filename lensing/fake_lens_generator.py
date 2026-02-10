32102a8cf68ad1c43c18785e9c406517a9708f44
Vidhi <vidhipratheesh@gmail.com>
2026-02-10 23:04:35 +0530

    Imporove fake lens generator and reduce overfitting

--- lensing/fake_lens_generator.py
+++ lensing/fake_lens_generator.py
@@ -1,51 +1,77 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import cv2
 import os
+import matplotlib.pyplot as plt
 
 os.makedirs("lensing/data/lensed",exist_ok=True)
 os.makedirs("lensing/data/unlensed",exist_ok=True)
 
-def generate_lensed(size=128):
-    img=np.zeros((size,size))
+SIZE=128
 
-    cx,cy=np.random.randint(40,88,size=2)
+def random_galexy_bg(size):
 
-    for i in range(size):
-        for j in range(size):
-            dist=np.sqrt((i-cx)**2 + (j-cy)**2)
-            if 18<dist<23:
-                img[i,j]=1
+    bg=np.random.normal(0.1,0.05,(size,size))
+
+    for _ in range(np.random.randint(3,8)):
+        x=np.random.randint(0,size)
+        y=np.random.randint(0,size)
+
+        cv2.circle(bg,(x,y),np.random.randint(1,3),0.8,-1)
+    return bg
+
+def generate_lensed():
+    
+    img=random_galexy_bg(SIZE)
+
+    cx=np.random.randint(40,88)
+    cy=np.random.randint(40,88)
+
+    radius=np.random.randint(18,25)
+    thickness=np.random.randint(3,7)
+
+    for i in range(SIZE):
+        for j in range(SIZE):
+            dist=np.sqrt((i-cx)**2+(j-cy)**2)
+
+            if radius<dist<radius+thickness:
+                brightness=np.random.uniform(0.7,1.2)
+                img[i,j]+=brightness
+    img=cv2.GaussianBlur(img,(5,5),0)
 
     img+=np.random.normal(0,0.05,img.shape)
 
-    return img
-
-def generate_unlensed(size=128):
-    img=np.zeros((size,size))
-     
-    cx,cy=size//2,size//2
-
-    for i in range(size):
-        for j in range(size):
-            dist=np.sqrt((i-cx)**2 + (j-cy)**2)
-
-            if dist<15:
-                img[i,j]=1
-# stronger random noise
-    img +=np.random.normal(0,0.15,img.shape)
-    # random brightness change
-    img=img*np.random.uniform(0.7,1.3)
-    # clip values (keep btw 0nd1)
     img=np.clip(img,0,1)
-    return img
-NUM_IMAGES = 50
 
-for i in range(NUM_IMAGES):
+    return img
+
+def generate_unlensed():
+
+    img=random_galexy_bg(SIZE)
+
+    cx=SIZE//2
+    cy=SIZE//2
+
+    r=np.random.randint(10,18)
+
+    cv2.circle(img,(cx,cy),r,0.6,-1)
+
+    img=cv2.GaussianBlur(img,(5,5),0)
+
+    img+=np.random.normal(0,0.05,img.shape)
+
+    img=np.clip(img,0,1)
+
+    return img
+
+NUM=100
+
+for i in range(NUM):
+
     lens=generate_lensed()
     normal=generate_unlensed()
 
     plt.imsave(f"lensing/data/lensed/lens_{i}.png",lens,cmap="inferno")
     plt.imsave(f"lensing/data/unlensed/normal_{i}.png",normal,cmap="inferno")
 
-print("Fake lensing dataset created 🚀")
 
+print("Advanced fake lens dataset created 🚀")
